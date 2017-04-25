494ece8636666a20ab3bd24dc988bdf42e602e93
Danesh <muqtaliff@gmail.com>
2017-04-26 00:27:20 +0530

    Notification fix

--- app/src/main/java/in/pleb/nadget/Notifier.java
+++ app/src/main/java/in/pleb/nadget/Notifier.java
@@ -45,7 +45,6 @@ public class Notifier {
             Log.i(TAG, "Notifier notifyTimeHr = "+notifyTimeHr);
             Log.i(TAG, "Notifier notifyTimeMin = "+notifyTimeMin);
 
-<<<<<<< HEAD
             //set the alarm to start at the said time
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(System.currentTimeMillis());
@@ -54,11 +53,6 @@ public class Notifier {
 
             alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                     AlarmManager.INTERVAL_DAY, alarmIntent);
-=======
-            //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, notifyTime, 86400000,alarmIntent);            
-            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + notifyTime, AlarmManager.INTERVAL_DAY, alarmIntent);
-            
->>>>>>> origin/master
         }
         else {
 
